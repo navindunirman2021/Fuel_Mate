753f219e4da8969a9b3713bf7e941bbfa439fbd8
Dulanjika <dulanjikabandara10@gmail.com>
2025-05-23 23:37:13 +0530

    implement nav-header.jsx, nav-main.jsx, nav-user.jsx, avatar.jsx
    Create title, the main navigation menu, user info and logout button.
    avata.jsx displays users' images / initials if no image is found
    (created by 15th May)

---
+++ Frontend/src/components/nav-header.jsx
@@ -0,0 +1,9 @@
+const NavHeader = () => {
+    return (
+        <div className="flex items-center justify-center p-4">
+            <h1 className="text-3xl font-bold text-teal-400 tracking-wide">Fuel Mate</h1>
+        </div>
+    )
+}
+
+export default NavHeader
