2f616bd5fbd89d066dd0899233a75eb1edfb7c97
Subhani-dilmini <dilminisubhani@gmail.com>
2025-05-23 15:18:23 +0000

    Add files via upload

---
+++ Backend/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.uokmit.fuelmaster.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private long timestamp;
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+
+}
