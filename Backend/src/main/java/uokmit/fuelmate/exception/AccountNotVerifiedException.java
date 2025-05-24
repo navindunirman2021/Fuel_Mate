1f1c3e423ba478da78a6071ba04f686230622df7
Subhani-dilmini <dilminisubhani@gmail.com>
2025-05-24 21:12:14 +0530

    add AccountNotVerifiedException

---
+++ Backend/src/main/java/uokmit/fuelmate/exception/AccountNotVerifiedException.java
@@ -0,0 +1,7 @@
+package com.uokmit.fuelmate.exception;
+
+public class AccountNotVerifiedException extends Exception {
+    public AccountNotVerifiedException(String message) {
+        super(message);
+    }
+}
