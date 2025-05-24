526fe72a16d6748f5bea48e2d88cead143db9aed
Subhani-dilmini <dilminisubhani@gmail.com>
2025-05-24 23:24:37 +0530

    edit MotorTrafficMockRepo

--- Backend/src/main/java/uokmit/fuelmate/repository/MotorTrafficMockRepo.java
+++ Backend/src/main/java/uokmit/fuelmate/repository/MotorTrafficMockRepo.java
@@ -12,17 +12,19 @@ public class MotorTrafficMockRepo {
 
     static {
         // Mock data for registered chassis numbers
-        registeredChassisNumbers.add("ABC12345");
+
+        registeredChassisNumbers.add("JKL98765");
+        registeredChassisNumbers.add("STU13579");
         registeredChassisNumbers.add("DEF67890");
         registeredChassisNumbers.add("GHI54321");
-        registeredChassisNumbers.add("JKL98765");
+        registeredChassisNumbers.add("ABC12345");
+        registeredChassisNumbers.add("YZA13568");
+        registeredChassisNumbers.add("EFG97531");
         registeredChassisNumbers.add("MNO43210");
         registeredChassisNumbers.add("PQR87654");
-        registeredChassisNumbers.add("STU13579");
         registeredChassisNumbers.add("VWX24680");
-        registeredChassisNumbers.add("YZA13568");
         registeredChassisNumbers.add("BCD86420");
-        registeredChassisNumbers.add("EFG97531");
+
     }
 
     public boolean isVehicleRegistered(String chassisNumber) {
