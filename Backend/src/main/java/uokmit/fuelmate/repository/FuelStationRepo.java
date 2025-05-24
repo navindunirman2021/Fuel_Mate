0718bae9ea471969274c2f164ce5569515dfa643
lakma1019 <161996821+lakma1019@users.noreply.github.com>
2025-05-24 14:11:11 +0530

    FuelStation repository with custom query methods.

---
+++ Backend/src/main/java/uokmit/fuelmate/repository/FuelStationRepo.java
@@ -0,0 +1,15 @@
+package com.uokmit.fuelmate.repository;
+
+import com.uokmit.fuelmate.Entity.FuelStation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FuelStationRepo extends JpaRepository<FuelStation, Long> {
+    boolean existsByRegNo(String regNo);
+    boolean existsByOwnerId(Long ownerId);
+
+    Optional<FuelStation> findById(Long id);
+
+    Optional<FuelStation> findByOwnerId(Long id);
+}
